8b00e2ca18720488349994b8a638ffd6824ccc83
Swaraj Das <swarajdas625w@gmail.com>
2024-07-15 13:54:31 +0530

    Created some changes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ menuList.style.maxHeight = "0px";
 
 function toggleMenu() {
     if (menuList.style.maxHeight == "0px") {
-        menuList.style.maxHeight = "154px";
+        menuList.style.maxHeight = "160px";
     } else {
         menuList.style.maxHeight = "0px";
     }
