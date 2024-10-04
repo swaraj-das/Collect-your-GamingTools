878b521f3a803d5225a1e1727420eebfd1e0cdaa
Sneha P Pratap <snehapratap248@gmail.com>
2024-10-04 21:30:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,36 @@ var menuList = document.getElementById("menuList");
 menuList.style.maxHeight = "0px";
 
 function toggleMenu() {
-    if (menuList.style.maxHeight == "0px") {
-        menuList.style.maxHeight = "160px";
-    } else {
-        menuList.style.maxHeight = "0px";
-    }
+  if (menuList.style.maxHeight == "0px") {
+    menuList.style.maxHeight = "160px";
+  } else {
+    menuList.style.maxHeight = "0px";
+  }
 }
+
+// Dark mode toggle
+const toggleButton = document.getElementById("darkModeToggle");
+const body = document.body;
+
+toggleButton.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+  document.querySelector(".navbar").classList.toggle("dark-mode");
+
+
+  if (body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark");
+    toggleButton.textContent = "☀️"; 
+  } else {
+    localStorage.setItem("theme", "light");
+    toggleButton.textContent = "🌙"; 
+  }
+});
+
+window.onload = function () {
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme && savedTheme === "dark") {
+    body.classList.add("dark-mode");
+    document.querySelector(".navbar").classList.add("dark-mode");
+    toggleButton.textContent = "☀️";
+  }
+};
