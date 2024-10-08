51252cf51e2d0fd7f2f576b7253a212513e40805
Shashank Goud <shashankgoud300@gmail.com>
2024-10-08 22:35:11 +0530

    Added Scroll Progress Bar

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ function toggleMenu() {
         menuList.style.maxHeight = "0px";
     }
 }
+
+ window.onscroll = function() {
+    updateProgressBar();
+  };
+  
+  
+  function updateProgressBar() {
+      var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+      var scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+      var scrollPercent = (scrollTop / scrollHeight) * 100;
+  
+      document.getElementById("progressBar").style.width = scrollPercent + "%";
+  }
+  
